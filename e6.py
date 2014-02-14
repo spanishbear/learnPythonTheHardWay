1adfc25b020e1b808274a095097eae0b110a7324
Beatriz Gomez <beatrizgomez@my.unt.edu>
2014-02-14 11:20:35 -0600

    Exercise 6: Strings and Text

--- e6.py
+++ e6.py
@@ -1,10 +1,6 @@
-#Bind x with the print statement and formatted variable
 x = "There are %d types of people." % 10
-# Bind binary with the string binary
 binary = "binary"
-# Bind do_not with the string don't 
 do_not = "don't"
-# Bind y with the string statement and 2 formatted variable
 y = "Those who know %s and those who %s." % (binary, do_not)
 
 print x
